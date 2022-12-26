380a23d3d504ed23cb588ad09f68d6c15cd4e041
davidgfb <davidgracia99@gmail.com>
2022-12-26 21:12:40 +0100

    Update Quad_Interior_Parallax_Mapping.py

--- Quad_Interior_Parallax_Mapping.py
+++ Quad_Interior_Parallax_Mapping.py
@@ -22,32 +22,6 @@ class App(WindowConfig):
 
             float dot2(in vec3 v) {return dot(v, v);}
 
-            float udQuad(vec3 v1, vec3 v2, vec3 v3, vec3 v4,
-                         vec3 p) {       
-                // handle ill formed quads
-                if(dot(cross(v2 - v1, v4 - v1),
-                   cross(v4 - v3, v2 - v3)) < 0) {
-                    vec3 tmp = v3;
-                    v3 = v4;
-                    v4 = tmp;
-                }
-                            
-                vec3 v21 = v2 - v1, p1 = p - v1, v32 = v3 - v2,
-                     p2 = p - v2, v43 = v4 - v3, p3 = p - v3,
-                     v14 = v1 - v4, p4 = p - v4,
-                     nor = cross(v21, v14);
-
-                return sqrt((sign(dot(cross(v21, nor), p1)) + 
-                       sign(dot(cross(v32, nor), p2)) + 
-                       sign(dot(cross(v43, nor), p3)) + 
-                       sign(dot(cross(v14, nor), p4)) < 3) ?
-                       min(min(dot2(v21 * clamp(dot(v21, p1) / dot2(v21), 0, 1) - p1), 
-                       dot2(v32 * clamp(dot(v32, p2) / dot2(v32), 0, 1) - p2)), 
-                       min(dot2(v43 * clamp(dot(v43, p3) / dot2(v43), 0, 1) - p3),
-                       dot2(v14 * clamp(dot(v14, p4) / dot2(v14), 0, 1) - p4))) :
-                       dot(nor, p1) * dot(nor, p1) / dot2(nor));
-            }
-
             float map(in vec3 p) {
                 vec3 v1 = vec3(1, 0,0), v2 = vec3(0,1,0),
                      v3 = vec3(0,0, 1), v4 = vec3(1,-1,1);
