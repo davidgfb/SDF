c770fdd6a1b14121aa538027fc9f1aaaae7a8046
davidgfb <davidgracia99@gmail.com>
2022-12-26 17:30:41 +0100

    Update Quad_Interior_Parallax_Mapping.py

--- Quad_Interior_Parallax_Mapping.py
+++ Quad_Interior_Parallax_Mapping.py
@@ -6,6 +6,7 @@ class App(WindowConfig):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         self.quad, self.program = quad_fs(),\
             self.ctx.program(vertex_shader =\
             '''#version 460
@@ -18,11 +19,11 @@ class App(WindowConfig):
             out vec4 fragColor; //color para cada fragmento
 
             uniform vec2 u_resolution;
-            uniform float h = 0;
 
             float dot2(in vec3 v) {return dot(v, v);}
 
-            float udQuad(vec3 v1, vec3 v2, vec3 v3, vec3 v4, vec3 p) {       
+            float udQuad(vec3 v1, vec3 v2, vec3 v3, vec3 v4,
+                         vec3 p) {       
                 // handle ill formed quads
                 if(dot(cross(v2 - v1, v4 - v1),
                    cross(v4 - v3, v2 - v3)) < 0) {
@@ -36,17 +37,15 @@ class App(WindowConfig):
                      v14 = v1 - v4, p4 = p - v4,
                      nor = cross(v21, v14);
 
-                return sqrt( (sign(dot(cross(v21,nor),p1)) + 
-                              sign(dot(cross(v32,nor),p2)) + 
-                              sign(dot(cross(v43,nor),p3)) + 
-                              sign(dot(cross(v14,nor),p4))<3.0) 
-                              ?
-                              min( min( dot2(v21*clamp(dot(v21,p1)/dot2(v21),0.0,1.0)-p1), 
-                                        dot2(v32*clamp(dot(v32,p2)/dot2(v32),0.0,1.0)-p2) ), 
-                                   min( dot2(v43*clamp(dot(v43,p3)/dot2(v43),0.0,1.0)-p3),
-                                        dot2(v14*clamp(dot(v14,p4)/dot2(v14),0.0,1.0)-p4) ))
-                              :
-                              dot(nor,p1)*dot(nor,p1)/dot2(nor) );
+                return sqrt((sign(dot(cross(v21, nor), p1)) + 
+                       sign(dot(cross(v32, nor), p2)) + 
+                       sign(dot(cross(v43, nor), p3)) + 
+                       sign(dot(cross(v14, nor), p4)) < 3) ?
+                       min(min(dot2(v21 * clamp(dot(v21, p1) / dot2(v21), 0, 1) - p1), 
+                       dot2(v32 * clamp(dot(v32, p2) / dot2(v32), 0, 1) - p2)), 
+                       min(dot2(v43 * clamp(dot(v43, p3) / dot2(v43), 0, 1) - p3),
+                       dot2(v14 * clamp(dot(v14, p4) / dot2(v14), 0, 1) - p4))) :
+                       dot(nor, p1) * dot(nor, p1) / dot2(nor));
             }
 
 
