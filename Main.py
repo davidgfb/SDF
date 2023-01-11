d2af8a94ea2d238a421db049c8102bf960e059c8
davidgfb <davidgracia99@gmail.com>
2023-01-11 18:53:15 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,99 +6,102 @@ class App(WindowConfig):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         self.quad, self.program = quad_fs(),\
-                                self.ctx.program(vertex_shader =\
-        '''#version 460
-        in vec3 in_position; //vertices pantalla
+            self.ctx.program(vertex_shader =\
+            '''#version 460
+            in vec3 in_position; //vertices pantalla
 
-        void main() {
-            gl_Position = vec4(in_position, 1); //rasteriza pantalla en fragmentos, vertices --> fragmentos
-        }''', fragment_shader =\
-        '''#version 460
-        out vec4 fragColor; //color para cada fragmento
+            void main() {
+                gl_Position = vec4(in_position, 1); //rasteriza pantalla en fragmentos, vertices --> fragmentos
+            }''', fragment_shader =\
+            '''#version 460
+            out vec4 fragColor; //color para cada fragmento
 
-        uniform vec2 u_resolution;
-        uniform float h = 0;
-   
-        float map(vec3 p, vec3 or = vec3(0),\
-                  vec3 fin = vec3(0, 4.0 / 10, 0),\
-                  float r = 3.0 / 10) {
-            vec3 pa = p - or, ba = fin - or;
-            float h = clamp(dot(pa, ba) / dot(ba, ba), 0, 1); //?
+            uniform vec2 u_resolution;
 
-            return length(-ba * h + pa) - r;
-        } //sdf capsula
+            float dot2(in vec3 v) {return dot(v, v);}
 
+            float map(in vec3 p) { //sdf quad               
+                vec3 v1 = vec3(1, 0, 0), v2 = vec3(0, 1, 0),
+                     v3 = vec3(0, 0, 1), v4 = vec3(1, -1, 1);
+                         
+                // handle ill formed quads
+                if(dot(cross(v2 - v1, v4 - v1),
+                   cross(v4 - v3, v2 - v3)) < 0) {
+                    vec3 tmp = v3;
+                    v3 = v4;
+                    v4 = tmp;
+                }
+                            
+                vec3 v21 = v2 - v1, p1 = p - v1, v32 = v3 - v2,
+                     p2 = p - v2, v43 = v4 - v3, p3 = p - v3,
+                     v14 = v1 - v4, p4 = p - v4,
+                     nor = cross(v21, v14);
 
-        /*float map(vec3 p) { //sdf esfera
-            return length(p) - 3.0 / 5; //u_resolution.y / u_resolution.x
-        }*/
-
-        vec3 getNormal(vec3 p, vec3 n = vec3(0, 1, 0)) { //sdf plano, en cualquier punto de la superficie del sdf el gradiente es el mismo que la normal del obj en ese punto 
-            float sharpness = 10; 
-            vec3 color = mix(vec3(0, 1, 0), vec3(1, 0, 0), clamp(sharpness * dot(p, n) - h, 0, 1)); //dot - h entre parentesis sino
-
-            return color;
-        }
-
-        vec3 get_P(float dist, vec3 rd, vec3 ro) {
-            return dist * rd + ro;
-        }
-
-        float rayMarch(vec3 ro, vec3 rd) {
-            float dist = 0;
-
-            for (int i = 0; i < 256; i++) {
-                float hit = map(get_P(dist, rd, ro)); //vector p, distancia mas corta al obj
-                dist += hit;
-
-                if (dist > 100 || abs(hit) < 1e-4) i = 256; //rompe bucle cuando esta lo suf cerca del obj o cuando el rayo escapa de la escena
+                return sqrt((sign(dot(cross(v21, nor), p1)) + 
+                       sign(dot(cross(v32, nor), p2)) + 
+                       sign(dot(cross(v43, nor), p3)) + 
+                       sign(dot(cross(v14, nor), p4)) < 3) ?
+                       min(min(dot2(v21 * clamp(dot(v21, p1) / dot2(v21), 0, 1) - p1), 
+                       dot2(v32 * clamp(dot(v32, p2) / dot2(v32), 0, 1) - p2)), 
+                       min(dot2(v43 * clamp(dot(v43, p3) / dot2(v43), 0, 1) - p3),
+                       dot2(v14 * clamp(dot(v14, p4) / dot2(v14), 0, 1) - p4))) :
+                       dot(nor, p1) * dot(nor, p1) / dot2(nor)) - 0.5; //0.01 truco    
             }
 
-            return dist;
-        }
+            float map1(vec3 p, vec3 or = vec3(0),\
+                  vec3 fin = vec3(0, 4.0 / 10, 0),\
+                  float r = 3.0 / 10) {
+                vec3 pa = p - or, ba = fin - or;
+                float h = clamp(dot(pa, ba) / dot(ba, ba), 0, 1); //?
 
-        vec3 render() { //color basado en la posicion del pixel en la pantalla
-            vec3 col = vec3(0), ro = vec3(0, 0, -1), rd = normalize(vec3((2 * gl_FragCoord.xy - u_resolution.xy) / u_resolution.y, 1)); //inicia negro, centra el origen de la pantalla, origen_Rayo, dir_Rayo
-            float dist = rayMarch(ro, rd); //dist al obj
+                return length(-ba * h + pa) - r;
+            } //sdf capsula
 
-            if (dist < 100) col += (getNormal(get_P(dist, rd, ro)) + 1) / 2; //suma la distancia al color resultante
-                        
-            return col;
-        }
+            vec3 get_P(float dist, vec3 rd, vec3 ro) {
+                return dist * rd + ro;
+            }
 
-        void main() {
-            fragColor = vec4(render(), 1);
-        }
-        ''') #pantalla
+            float opSubtraction(float d1, float d2) {
+                return max(-d1, d2);
+            }
+            
+            float opUnion(float d1, float d2) {
+                return min(d1,d2);
+            }
+
+            float get_Impacto(float dist, vec3 rd, vec3 ro) {
+                vec3 impacto = get_P(dist, rd, ro);
+                
+                //return opUnion(map(impacto), map1(impacto));
+                return opSubtraction(map(impacto), map1(impacto));
+            }
+
+            void main() { //color basado en la posicion del pixel en la pantalla 
+                vec3 col = vec3(0), ro = vec3(0, 0, -1),
+                     rd = normalize(vec3((2 * gl_FragCoord.xy - u_resolution.xy) /
+                         u_resolution.y, 1)); //inicia negro, centra el origen de la pantalla, origen_Rayo, dir_Rayo
+
+                float dist = 0;
+
+                for (int i = 0; i < 256; i++) {
+                    float hit = get_Impacto(dist, rd, ro); //vector p, distancia mas corta al obj
+                    dist += hit;
+
+                    if (dist > 100 || abs(hit) < 1e-4) i = 256; //rompe bucle cuando esta lo suf cerca del obj o cuando el rayo escapa de la escena
+                }
+
+                if (dist < 100) col += (get_Impacto(dist, rd, ro) + 1) / 2; //suma la distancia al color resultante
+                
+                fragColor = vec4(col, 1);
+            }''') #pantalla
+
         self.program['u_resolution'] = self.window_size
         
     def render(self, time, frame_time):
         self.ctx.clear()       
         self.quad.render(self.program) #uniform tiene q estar inicializado en glsl
 
-    def unicode_char_entered(self, char):
-        global h
-
-        es_Mas = char == '+'
-        
-        if es_Mas or char == '-':
-            if es_Mas:
-                h += 1
-
-            else:
-                h -= 1
-
-            self.program['h'] = h            
-
-    '''def mouse_position_event(self, x, y, dx, dy):
-        print(x, y)
-        #self.program['u_mouse'] = (x, y)'''
-
 h, es_Primera_Vez = 0, True
 run_window_config(App)
-
-
-
-
-        
